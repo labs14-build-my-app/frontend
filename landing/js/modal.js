37024faba22cb3989b9623041fe6fad44b8cd461
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-07-26 20:15:03 -0700

    updated to reflect UX designer

--- landing/js/modal.js
+++ landing/js/modal.js
@@ -6,14 +6,17 @@ const hamburger = document.querySelector(".hamburger-svg");
 //css
 const modalCSS = ["blur(3px) brightness(35%)", ""]
 const navCSS = ["none", "flex"]
+//end css
+
 
 const displayModal = (e) => {
 modal.style.filter =  modalCSS[0];
 
 nav.style.display = navCSS[1];
-hamburger.style.display = navCSS[0]
-
+hamburger.style.display = navCSS[0];
+hamburger.parentElement.children[0].style.display = navCSS[0];
 nav.addEventListener("click", removeModal);
+window.addEventListener("scroll", preventScroll);
 
 
 }
@@ -21,12 +24,15 @@ const removeModal = (e) =>{
     modal.style.filter = modalCSS[1];
     nav.style.display = navCSS[0];
     hamburger.style.display = navCSS[1];
+    hamburger.parentElement.children[0].style.display = navCSS[1];
+    window.removeEventListener("scroll", preventScroll);
+}
 
-  
+
+const preventScroll = (e) =>{
+    window.scrollTo(1,1);
 }
 
 hamburger.addEventListener("click", displayModal);
+window.addEventListener("scroll", preventScroll);
 
-//on click, hamburger menu displays none.
-//Close button displays from hamburger and div 
-//home-navigation-bar displays
