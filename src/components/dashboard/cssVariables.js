1a112926b82342f414ba4f4e4675afa635be0d11
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-07-29 16:20:14 -0700

    comments

--- src/components/dashboard/cssVariables.js
+++ src/components/dashboard/cssVariables.js
@@ -1,3 +1,8 @@
+/*
+ATTENTION ALL GAMERS
+use http://chir.ag/projects/name-that-color/#9519E8 
+to name your colors. Thanks for coding responsively.
+*/
 export const sunglow  = '#FFDC2B';
 export const shamrock = '#25DA95';
 export const electricViolet = '#9519E8';
