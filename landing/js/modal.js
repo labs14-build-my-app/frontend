1618296c2f4b029dc532d1771e2a05b3c2194d0a
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-07-28 18:53:02 -0700

    animation for desktop-modal

--- landing/js/modal.js
+++ landing/js/modal.js
@@ -1,31 +1,81 @@
 //DOM elements
 const modal = document.querySelector(".hamburger-modal");
-const nav = document.querySelector(".modal-nav-elements");
+const mobileNav = document.querySelector(".modal-nav-elements");
 const hamburger = document.querySelector(".hamburger-svg");
 
+const desktopNav = hamburger.parentNode.querySelector("ul");
+const introGetStarted = document.querySelector('.cta-getstarted-section').querySelector('h2');
+
+const closeButtonSrc = "./assets/svgs/icon_close.svg";
+const hamburgerButtonSrc = "./assets/svgs/icon_Menu.svg";
 //css
-const modalCSS = ["blur(3px) brightness(35%)", ""]
-const navCSS = ["none", "flex"]
+const mobileModalCSS = ["blur(3px) brightness(35%)", ""]
+const mobileNavCSS = ["none", "flex"]
 //end css
+const desktopNavCSS = ["none", "flex"];
+
+const opacityAnimationDuration = 300;//milliseconds
+
+const displayNav = (e) => {
+
+const isMobile = window.getComputedStyle(introGetStarted, null).getPropertyValue("font-size")==="23px";
+
+    if(isMobile){  
+        modal.style.filter =  mobileModalCSS[0];
+        mobileNav.addEventListener("click", removeNav);
+        mobileNav.style.display = mobileNavCSS[1];
+        hamburger.style.display = mobileNavCSS[0];
+        hamburger.parentElement.children[0].style.display = mobileNavCSS[0];
+       
+        
+        window.addEventListener("scroll", preventScroll);
+    }
+    else{
+       
+        desktopNav.style.display = desktopNavCSS[1];
+        hamburger.src = closeButtonSrc;
+        hamburger.parentElement.children[1].textContent = "CLOSE";
+        hamburger.removeEventListener("click", displayNav);
+        hamburger.addEventListener("click", removeNav);
+        
+        hamburger.parentElement.classList.add("displayNavAnime");
+        setTimeout(removeAnimation, opacityAnimationDuration);
+    }
 
 
-const displayModal = (e) => {
-modal.style.filter =  modalCSS[0];
-
-nav.style.display = navCSS[1];
-hamburger.style.display = navCSS[0];
-hamburger.parentElement.children[0].style.display = navCSS[0];
-nav.addEventListener("click", removeModal);
-window.addEventListener("scroll", preventScroll);
 
 
 }
-const removeModal = (e) =>{
-    modal.style.filter = modalCSS[1];
-    nav.style.display = navCSS[0];
-    hamburger.style.display = navCSS[1];
-    hamburger.parentElement.children[0].style.display = navCSS[1];
-    window.removeEventListener("scroll", preventScroll);
+const removeNav = (e) =>{
+
+
+    const isMobile = window.getComputedStyle(introGetStarted, null).getPropertyValue("font-size")==="23px";
+        if(isMobile){
+            modal.style.filter = mobileModalCSS[1];
+            mobileNav.style.display = mobileNavCSS[0];
+            hamburger.style.display = mobileNavCSS[1];
+            hamburger.parentElement.children[0].style.display = mobileNavCSS[1];
+            window.removeEventListener("scroll", preventScroll);
+        }
+        else{
+            
+            hamburger.src = hamburgerButtonSrc;
+            desktopNav.style.display = desktopNavCSS[0];
+          
+            hamburger.parentElement.children[1].textContent = "MENU"
+
+            hamburger.parentElement.classList.add("displayNavAnime")
+            
+            hamburger.removeEventListener("click", removeNav);
+            hamburger.addEventListener("click", displayNav);
+
+            setTimeout(removeAnimation, opacityAnimationDuration);
+        }
+
+}
+
+const removeAnimation = () =>{
+    hamburger.parentElement.classList.remove("displayNavAnime");
 }
 
 
@@ -33,6 +83,5 @@ const preventScroll = (e) =>{
     window.scrollTo(1,1);
 }
 
-hamburger.addEventListener("click", displayModal);
-window.addEventListener("scroll", preventScroll);
+hamburger.addEventListener("click", displayNav);
 
