4211d640b640dcff2fc76e7b03425d73b6aa6cec
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-07-29 16:48:23 -0700

    project css update

--- src/components/dashboard/ProjectBoard/ProjectList.js
+++ src/components/dashboard/ProjectBoard/ProjectList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Project from "./Project";
-import styled from "styled-components";
 
 export default class ProjectList extends Component {
   render() { 
