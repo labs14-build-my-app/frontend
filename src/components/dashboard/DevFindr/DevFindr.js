a843307fa8180466d5f13f55752217a5fc9f520f
Alfonso <alfonso.garcia2324@gmail.com>
2019-08-13 09:33:12 -0700

    Fixed error in rendering statement and notification

--- src/components/dashboard/DevFindr/DevFindr.js
+++ src/components/dashboard/DevFindr/DevFindr.js
@@ -1,31 +1,39 @@
-import React from 'react'
+import React from "react";
 import DeveloperDashboard from "../Developer/DeveloperDashboard";
 import EntrepreneurBoard from "../Entrepreneur/EntrepreneurBoard";
-import {connect} from "react-redux";
-import {loadApp } from "../../../redux/actions"
-const errorHasOccured = <p>An Error has occured please log in again</p>
+import { connect } from "react-redux";
+import { loadApp } from "../../../redux/actions";
+const errorHasOccured = <p>An Error has occured please log in again</p>;
 
-const DevFindr = (props) =>{
-    console.log(props)
-    if( !props.user ){
-        console.log("loading app")
-        props.loadApp();
-    }
-        return (
-            props.user ? 
-            
-            
-            <>
-            {   console.log(!props.user.isDeveloper === false)}
-            {console.log(props.user)}
-               {props.user.isDeveloper ? <DeveloperDashboard {...props} /> : !props.user.isDeveloper === true ? <EntrepreneurBoard {...props} /> : errorHasOccured}    
-            </> : errorHasOccured
-        )
-}
-const mapStateToProps = state =>{
-    console.log(state)
-    return {
+const DevFindr = props => {
+  // console.log(props)
+  if (!props.user) {
+    console.log("loading app");
+    props.loadApp();
+  }
+  return props.user ? (
+    <>
+      {console.log(!props.user.isDeveloper === false)}
+      {console.log(props.user)}
+      {props.user.isDeveloper ? (
+        <DeveloperDashboard {...props} />
+      ) : !props.user.isDeveloper === true ? (
+        <EntrepreneurBoard {...props} />
+      ) : (
+        errorHasOccured
+      )}
+    </>
+  ) : (
+    errorHasOccured
+  );
+};
+const mapStateToProps = state => {
+  // console.log(state)
+  return {
     user: state.user
-    }
-} 
-export default connect(mapStateToProps, { loadApp })( DevFindr)
+  };
+};
+export default connect(
+  mapStateToProps,
+  { loadApp }
+)(DevFindr);
