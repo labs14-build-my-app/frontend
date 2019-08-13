efcbd29487728b838b8b2feb24491c67ae760e52
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-13 09:50:41 -0700

    cleared styling

--- src/components/dashboard/Login/Login.js
+++ src/components/dashboard/Login/Login.js
@@ -6,115 +6,9 @@ import { BeatLoader } from "react-spinners";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { phone, tablet } from "../cssVariables";
-const LoginContainer = styled.div`
-  .__Form {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
 
-    input {
-      font-size: 16px;
-      margin-bottom: 24px;
-      padding: 8px;
-    }
-
-    button {
-      font-size: 16px;
-      margin-bottom: 24px;
-      padding: 8px;
-      text-transform: uppercase;
-      font-weight: bold;
-      color: white;
-      background: black;
-    }
-  }
-
-  .cta-content {
-    font-size: 3.5rem;
-    line-height: 2;
-    width: 90%;
-    margin: 0 auto;
-    algin-self: center;
-  }
-
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-
-  width: 85%;
-  max-width: 962px;
-  // display: flex;
-  margin: 4em auto;
-
-  align-items: center;
-  flex-direction: row-reverse;
-  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
-  border-radius: 4px;
-  background-color: #fff;
-
-  @media (max-width: 1081px) {
-    flex-direction: column;
-    width: 90%;
-    box-shadow: none;
-    .cta-content {
-      ${"" /* display: none; */}
-      h1 {
-        font-size: 3.5rem;
-        line-height: 2;
-        width: 90%;
-      }
-    }
-  }
-  .cta-content {
-    width: 60%;
-    padding: 0 2em;
-    h1 {
-    }
-  }
-  .login-main {
-    width: 40%;
-    border-right: 1px solid #efefef;
-    padding: 1em;
-    margin: 0 auto;
-    padding: 4em 2em;
-    @media ${tablet} {
-      border: 0px;
-      width: 60%;
-    }
-    @media ${phone} {
-      width: 100%;
-    }
-    .alternative-cta {
-      width: 90%;
-      a {
-        color: #0993fc;
-        text-decoration: none;
-      }
-    }
-    form {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-
-      input {
-        font-size: 16px;
-        margin-bottom: 20px;
-        padding: 8px;
-      }
-
-      button {
-        font-size: 16px;
-        margin-bottom: 24px;
-        padding: 8px;
-        text-transform: uppercase;
-        font-weight: bold;
-        color: white;
-        background: red;
-      }
-    }
-  }
-`;
+// const LoginContainer = styled.div`
+// `;
 
 class Login extends Component {
   state = {
@@ -135,7 +29,7 @@ class Login extends Component {
     // });
     //if user.type === dev
     this.props.login({ ...this.state }).then((res)=>{
-      console.log(res)
+      console.log('Login.js Says: ' + res)
       this.props.history.push("/");
     });
    
@@ -194,6 +88,7 @@ const mapStateToProps = state => {
     loggingIn: state.loggingIn
   };
 };
+
 export default connect(
   mapStateToProps,
   { login }
