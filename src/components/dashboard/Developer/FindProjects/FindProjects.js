b9e59f44c9d902b115c0e0f8957e0da6979c5d54
Alfonso <alfonso.garcia2324@gmail.com>
2019-08-13 14:43:20 -0700

    Added truthful counter to project page

--- src/components/dashboard/Developer/FindProjects/FindProjects.js
+++ src/components/dashboard/Developer/FindProjects/FindProjects.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import FindProjectsSearchBar from "./FindProjectsSearchBar";
 import styled from "styled-components";
 const FindProjectsContainer = styled.div`
@@ -6,10 +7,10 @@ const FindProjectsContainer = styled.div`
     display: flex;
     justify-content: space-around;
     margin-bottom: 5.125em;
-    .all-project-btn, .saved-project-btn{
-    font-size: 1.8rem;
-    color: #9592A8;
-    
+    .all-project-btn,
+    .saved-project-btn {
+      font-size: 1.8rem;
+      color: #9592a8;
     }
     .all-project-btn {
       margin-right: 2.1875em;
@@ -36,23 +37,24 @@ const FindProjectsContainer = styled.div`
       width: 100%;
     }
   }
-    .find-proj-description{
-        display: flex;
-        justify-content: space-between;
-        h2{
-            font-size: 1.8rem;
-            color: #9592A8;
-        }
-        button{
-           display: flex;
-           align-items: center;
-            p,img{
-                margin: 0 .5em;
-                font-size: 1.3rem;
-                text-transform: uppercase;
-            }
-        }
+  .find-proj-description {
+    display: flex;
+    justify-content: space-between;
+    h2 {
+      font-size: 1.8rem;
+      color: #9592a8;
     }
+    button {
+      display: flex;
+      align-items: center;
+      p,
+      img {
+        margin: 0 0.5em;
+        font-size: 1.3rem;
+        text-transform: uppercase;
+      }
+    }
+  }
 `;
 export class FindProjects extends Component {
   render() {
@@ -65,11 +67,16 @@ export class FindProjects extends Component {
           <button className="saved-project-btn">SAVED PROJECTS</button>
         </div>
         <div className="find-proj-description">
-          <h2>There are 1,000,542 projects open for project proposals.</h2>
+          <h2>
+            There are {this.props.projectList.length} projects open for project
+            proposals.
+          </h2>
           <button>
-          <p>refresh </p>
-          <img src={process.env.PUBLIC_URL + "/images/redo-alt.svg"} alt="refresh"/>
-
+            <p>refresh </p>
+            <img
+              src={process.env.PUBLIC_URL + "/images/redo-alt.svg"}
+              alt="refresh"
+            />
           </button>
         </div>
       </FindProjectsContainer>
@@ -77,4 +84,13 @@ export class FindProjects extends Component {
   }
 }
 
-export default FindProjects;
+const mapStateToProps = state => {
+  return {
+    projectList: state.projectList
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(FindProjects);
