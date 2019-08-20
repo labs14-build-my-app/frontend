54f4a6ef79e7eb1b9b63508ddf56505c57ef1aac
IlanL75 <anatoly.leytman@gmail.com>
2019-08-20 10:25:58 -0700

    style partial rework

--- src/components/dashboard/Login/Signup.js
+++ src/components/dashboard/Login/Signup.js
@@ -5,30 +5,40 @@ import { NavLink, Link } from "react-router-dom";
 import { signup } from "../../../redux/actions";
 
 import styled from "styled-components";
-import { headerFontDesktop, h1, electricViolet, veryLightBlue, textColor1, textColor2, textBox } from "../cssVariables";
+import { headerFontDesktop, h1, electricViolet, veryLightBlue, textColor1, textColor2, losLinks, projectFontSubtext, losTransitionTime, losBackground, privilege } from "../cssVariables";
 import { BeatLoader } from "react-spinners";
 
-const LoginPageContainer = styled.div`
-  border: 1px solid red;
+const SignupPageContainer = styled.div`
   margin: 0 auto;
   display: flex;
   display: nowrap;
   font-size: ${headerFontDesktop};
-  height: 1080px; // temporary
+  height: 100%;
+  a{
+    text-decoration: none;
+    color: ${textColor2};
+      :visited{
+        text-decoration: none;
+      }
+      :hover{
+        color: ${electricViolet};
+        transition: ${losTransitionTime};
+      }
+  }
 `;
 
 const LeftContainer = styled.div`
   width: 45%;
   height: 100%;
-  background: #F2F3FF;
+  background: ${veryLightBlue};
 
   .l-container{
-    margin: 17rem 15.5rem 0;
+    width: calc(100% - 144px);
+    margin: 0 auto;
     height: 100%;
 
-    border: 1px solid black;
-
     nav{
+      padding-top: 5.0625em
 
       ol{
         list-style-type: none;
@@ -37,25 +47,21 @@ const LeftContainer = styled.div`
 
       li{
         display: inline-block;
+        font-size: ${losLinks};
 
-        :first-child{
-          font-size: 2.5rem;
+        .active{
+          color: ${electricViolet};
         }
-
         :nth-child(2){
-          font-size: 1.3rem;
+          font-size: ${projectFontSubtext};
           padding: 0 1.8rem 0;
+          color: ${textColor2};
         }
-
-        :nth-child(3){
-          font-size: 2.5rem;
-        }
-
       }
     }
 
     .form{
-      font-size: 1.8rem;
+      font-size: ${headerFontDesktop};
       margin: 20.3rem 0 0;
 
 
@@ -71,6 +77,7 @@ const LeftContainer = styled.div`
           position: relative;
           float: left;
           width: 100%;
+          margin: 0 0 3rem;
 
           .field-title{
             display: inline-block;
@@ -78,35 +85,54 @@ const LeftContainer = styled.div`
           }
 
           input{
-            width: 100%
-            box-sizing: border-box; // fixes the issue where it expands out of the box model by 1.3px on the right
+            width: 100%;
+            height: 4.2rem; /* prevents text from clipping*/
+            box-sizing: border-box; /* fixes the issue where it expands out of the box model by 1.3px on the right */
             background: transparent;
             border: 0;
-            padding: 0 .5rem 2rem;
+            padding: 0 0 2rem 0rem;
             border-bottom: 2px solid ${textColor1};
-
+            font-size: ${headerFontDesktop};
             ::placeholder{
               font-size: ${headerFontDesktop};
             }
-
           }
 
-          // animation's for the text-box bottom border
+          /* password visibility button */
+          .password-visibility{
+            position: absolute;
+            float: right;
+            top: 4.5rem;
+            right: 2rem;
+            z-index: 1;
+          }
+
+          /* animation's for the text-box bottom border */
 
           .input-box ~ .input-border{
             position: absolute;
             bottom: 0;
             left: 0;
             width: 0;
-            height: .26rem; // makes a slightly visable diffrence that lets users know they have clicked the text box
+            height: .26rem;
             background-color: ${electricViolet};
-            transition: 0.4s;}
-
+            transition: ${losTransitionTime};
+            }
           .input-box:focus ~ .input-border{
             width: 100%;
-            transition: 0.4s;
+            transition: ${losTransitionTime};
           }
 
+        }
+
+        .submit-button{
+          margin: 1rem 0 0;
+          font-size: 1.6rem;
+          color: #fff;
+          background: ${electricViolet};
+          box-shadow: 5px 5px 8px rgba(149, 25, 232, 0.16);
+          border-radius: .6rem;
+          height: 5.5rem;
         }
 
         }
@@ -119,38 +145,29 @@ const LeftContainer = styled.div`
 const RightContainer = styled.div`
   width: 55%;
   height: 100%;
-
   .r-container{
-    height: 100%;
-    background:
-    /* gradient overlay */
-    linear-gradient(61deg, rgba(24, 7, 94, .7), rgba(149, 25, 232, .7)),
-    /* bottom image */
-    url('${process.env.PUBLIC_URL + "/images/heros/login-signup-hero.jpg"}');
-
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-
+     height: 100vh;
+    
+    ${losBackground};
     .text{
       padding-top: 40%;
-      color: white;
+      color: ${privilege};
       margin-left: 9rem;
-
       h1{
         font-size: ${h1};
         line-height: 6.9rem;
         padding: 0 0 2rem;
       }
-
       p{
         line-height: 3rem;
       }
-
-
     }
   }
 `;
+
+const showPass = <img src={process.env.PUBLIC_URL + '/images/icons/icon_Eye_slash.svg'} alt="show password" />;
+const hidePass = <img src={process.env.PUBLIC_URL + '/images/icons/icon_Eye_visable.svg'} alt="hide password" />;
+
 
 class Signup extends Component {
   state = {
@@ -167,12 +184,19 @@ class Signup extends Component {
     });
   };
 
+  togglePasswordVisibility = e => {
+    e.preventDefault();
+    this.setState({
+      password_hidden: !this.state.password_hidden
+    })
+  };
+
   
 
   render() {
 
     return (
-      <LoginPageContainer className="page-container">
+      <SignupPageContainer className="page-container">
         <LeftContainer>
           <div className="l-container">
             <nav>
@@ -183,7 +207,7 @@ class Signup extends Component {
               </ol>
             </nav>
             <div className='form'>
-              <form onSubmit={this.handleSubmit}>
+              <form>
 
                 <div className="input-field">
                   <span className="field-title"> First Name </span> <br />
@@ -242,7 +266,7 @@ class Signup extends Component {
             </div>
           </div>
         </RightContainer>
-      </LoginPageContainer>
+      </SignupPageContainer>
     );
   }
 }
--- src/components/dashboard/Login/Signup2.js
+++ src/components/dashboard/Login/Signup2.js
@@ -5,30 +5,40 @@ import { NavLink } from "react-router-dom";
 import { signup } from "../../../redux/actions";
 
 import styled from "styled-components";
-import { headerFontDesktop, h1, electricViolet, veryLightBlue, textColor1, textColor2, textBox } from "../cssVariables";
+import { headerFontDesktop, h1, electricViolet, veryLightBlue, textColor1, textColor2, losLinks, projectFontSubtext, losTransitionTime, losBackground, privilege } from "../cssVariables";
 import { BeatLoader } from "react-spinners";
 
-const LoginPageContainer = styled.div`
-  border: 1px solid red;
+const Signup2PageContainer = styled.div`
   margin: 0 auto;
   display: flex;
   display: nowrap;
   font-size: ${headerFontDesktop};
-  height: 1080px; // temporary
+  height: 100%;
+  a{
+    text-decoration: none;
+    color: ${textColor2};
+      :visited{
+        text-decoration: none;
+      }
+      :hover{
+        color: ${electricViolet};
+        transition: ${losTransitionTime};
+      }
+  }
 `;
 
 const LeftContainer = styled.div`
   width: 45%;
-  height: 100%;
-  background: #F2F3FF;
+  height: 100%vh;
+  background: ${veryLightBlue};
 
   .l-container{
-    margin: 17rem 15.5rem 0;
+    width: calc(100% - 144px);
+    margin: 0 auto;
     height: 100%;
 
-    border: 1px solid black;
-
     nav{
+      padding-top: 5.0625em
 
       ol{
         list-style-type: none;
@@ -37,81 +47,20 @@ const LeftContainer = styled.div`
 
       li{
         display: inline-block;
+        font-size: ${losLinks};
 
-        :first-child{
-          font-size: 2.5rem;
+        .active{
+          color: ${electricViolet};
         }
-
         :nth-child(2){
-          font-size: 1.3rem;
+          font-size: ${projectFontSubtext};
           padding: 0 1.8rem 0;
+          color: ${textColor2};
         }
-
-        :nth-child(3){
-          font-size: 2.5rem;
-        }
-
       }
     }
 
-    .form{
-      font-size: 1.8rem;
-      margin: 20.3rem 0 0;
-
-
-      input:focus{
-        outline: none;
-      }
-
-      form{
-        display: flex;
-        flex-direction: column;
-
-        .input-field{
-          position: relative;
-          float: left;
-          width: 100%;
-
-          .field-title{
-            display: inline-block;
-            margin: 0 0 3rem;
-          }
-
-          input{
-            width: 100%
-            box-sizing: border-box; // fixes the issue where it expands out of the box model by 1.3px on the right
-            background: transparent;
-            border: 0;
-            padding: 0 .5rem 2rem;
-            border-bottom: 2px solid ${textColor1};
-
-            ::placeholder{
-              font-size: ${headerFontDesktop};
-            }
-
-          }
-
-          // animation's for the text-box bottom border
-
-          .input-box ~ .input-border{
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 0;
-            height: .26rem; // makes a slightly visable diffrence that lets users know they have clicked the text box
-            background-color: ${electricViolet};
-            transition: 0.4s;}
-
-          .input-box:focus ~ .input-border{
-            width: 100%;
-            transition: 0.4s;
-          }
-
-        }
-
-        }
-
-      }
+    
     }
   }
 `;
@@ -119,35 +68,22 @@ const LeftContainer = styled.div`
 const RightContainer = styled.div`
   width: 55%;
   height: 100%;
-
   .r-container{
-    height: 100%;
-    background:
-    /* gradient overlay */
-    linear-gradient(61deg, rgba(24, 7, 94, .7), rgba(149, 25, 232, .7)),
-    /* bottom image */
-    url('${process.env.PUBLIC_URL + "/images/heros/login-signup-hero.jpg"}');
-
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-
+     height: 100vh;
+    
+    ${losBackground};
     .text{
       padding-top: 40%;
-      color: white;
+      color: ${privilege};
       margin-left: 9rem;
-
       h1{
         font-size: ${h1};
         line-height: 6.9rem;
         padding: 0 0 2rem;
       }
-
       p{
         line-height: 3rem;
       }
-
-
     }
   }
 `;
@@ -174,7 +110,7 @@ class Signup2 extends Component {
         console.log(this.props);
         console.log(this.state);
         return (
-            <LoginPageContainer className="page-container">
+          <Signup2PageContainer className="page-container">
                 <LeftContainer>
                     <div className="l-container">
                         <nav>
@@ -206,7 +142,7 @@ class Signup2 extends Component {
                         </div>
                     </div>
                 </RightContainer>
-            </LoginPageContainer>
+          </Signup2PageContainer>
         );
     }
 }
