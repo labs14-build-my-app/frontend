30eaf83ca0665eb6ae47ca0e5171ab55a761df89
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-21 20:29:05 -0700

    Put some noodles in the pan, but i forgot the water. Its not spaghet time yet

--- src/components/dashboard/Developer/Dashboard/Projects Loader/ProjectsList.jsx
+++ src/components/dashboard/Developer/Dashboard/Projects Loader/ProjectsList.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import ProjectsLoader from './ProjectsLoader';
 
+import {tempstate} from './tempstate';
+
+// This is a HOC which takes in an array of projects a signed in developer has.
+// It then gets passed to ProjectsLoader.jsx as props so it can be returned and rendered on the the Projects List
+
 class ProjectsList extends Component {
+
+	state = {tempstate}
+
 	render() { 
 		return (
-			<div className="">
+			<div className="projects-list">
 				Projects List lives here <br/>
 				<ProjectsLoader />
 			</div>
