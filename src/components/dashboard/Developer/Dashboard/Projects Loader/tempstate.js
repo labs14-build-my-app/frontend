d7e7efc17925a2d0af1cc5d8a0c4db7c0402ac26
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-22 20:53:52 -0700

    added more projects

--- src/components/dashboard/Developer/Dashboard/Projects Loader/tempstate.js
+++ src/components/dashboard/Developer/Dashboard/Projects Loader/tempstate.js
@@ -5,7 +5,7 @@ export const tempstate = {
 			"ownerName": "Alfonso Garcia",
 			"owner": "5d5dd28cf58b080017a4db57",
 			"name": "Not DevFindr v2",
-			"price": "50000",
+			"price": 50000,
 			"status": "In Progress",
 		},
 		{
@@ -13,16 +13,40 @@ export const tempstate = {
 			"ownerName": "Joseph Jalapeno",
 			"owner": "5d5dd28cf58b080017a4db57",
 			"name": "Social Manager for Influencers",
-			"price": "3000",
+			"price": 3000,
 			"status": "Completed",
+		},
+		{
+			"ownerProfilePicture": "https://thispersondoesnotexist.com/image",
+			"ownerName": "Chris Doe",
+			"owner": "5d5dd28cf58b080017a4db57",
+			"name": "Client Manager",
+			"price": 308442,
+			"status": "cancelled",
+		},
+		{
+			"ownerProfilePicture": "https://thispersondoesnotexist.com/image",
+			"ownerName": "Alexis Garcia",
+			"owner": "5d5dd28cf58b080017a4db57",
+			"name": "LambdaSchool Student Management",
+			"price": 53423156,
+			"status": "cancelled",
+		},
+		{
+			"ownerProfilePicture": "https://thispersondoesnotexist.com/image",
+			"ownerName": "Lilliana Splenduval",
+			"owner": "5d5dd28cf58b080017a4db57",
+			"name": "Big Picture student manager",
+			"price": 352235,
+			"status": "cancelled",
 		},
 		{
 			"ownerProfilePicture": "https://thispersondoesnotexist.com/image",
 			"ownerName": "Junior Buttsnack",
 			"owner": "5d5dd28cf58b080017a4db57",
 			"name": "The Snack That Smiles Back",
-			"price": "53156",
+			"price": 53156,
 			"status": "completed",
-		}
+		},
 	]
 }
