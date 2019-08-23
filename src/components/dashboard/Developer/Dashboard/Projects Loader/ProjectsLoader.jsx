3abc287fc24c7a87095cfb3cb4b9a6bdca72d3c4
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-22 19:59:30 -0700

    Rendering stuff from temp state

--- src/components/dashboard/Developer/Dashboard/Projects Loader/ProjectsLoader.jsx
+++ src/components/dashboard/Developer/Dashboard/Projects Loader/ProjectsLoader.jsx
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { renderComponent } from 'recompose';
 
+const Img = styled.img`
+width: 50px;
+`
+
 const ProjectsLoader = (props) => {
 	return (
-		<p> {props.name} </p>
+		<div className="project">
+			<Img src={props.ownerProfilePicture} alt="I promise this person doesnt exist"/>
+			<p> {props.name} </p>
+			<p> {props.ownerName}</p>
+
+			<p> {props.price} </p>
+			<p> {props.status} </p>
+		</div>
 	)
 }
  
