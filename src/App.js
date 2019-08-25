dbaf296fdf3083abc4b3cabce3025a45f2a6da1e
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-24 17:36:55 -0700

    working on figuring out this failed to compile error. Shoot me please right now

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import React from "react";
 import "./index.css";
 import { Route, Switch } from "react-router-dom";
-import Login from "./components/dashboard/Login/Login";
 import PrivateRoute from "./components/auth/PrivateRoute";
-import Signup from "./components/dashboard/Login/Signup";
-import Signup2 from "./components/dashboard/Login/Signup2";
+import styled, { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+
+import Login from "./components/Dashboard/Authentication/Login/Login";
+import ChangePassword from "./components/Dashboard/Authentication/ChangePassword";
+
+import Signup from "./components/Dashboard/Authentication/Signup";
+import Signup2 from "./components/Dashboard/Authentication/Signup2";
 
 import DashboardChooser from "./components/dashboard/AccountTypes/DashboardChooser";
-import ChangePassword from "./components/dashboard/Login/ChangePassword";
+
+
 import Header from "./components/dashboard/Developer/Header/Header";
-import styled, { ThemeProvider } from "styled-components";
 import LeftNavigation from "./components/dashboard/Navigation/LeftNavigation";
-import { useSelector } from "react-redux";
-const AppContainer = styled.div`
-  ${props =>
-    props.theme.history.location.pathname.startsWith("/dev") && "height: 100%"};
-  .essential-container {
-    display: flex;
-    height: 100%;
-  }
 
-  .main-app-container {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-
-    .main-app-column {
-      background: transparent linear-gradient(297deg, #f2f3ff 0%, #ffffff 100%);
-      .developer-home {
-        padding: 11.5625em 7.125em 7em 6.625em;
-      }
-    }
-  }
-`;
 const App = props => {
   console.log(props);
 
@@ -68,3 +52,25 @@ const App = props => {
 };
 
 export default App;
+
+const AppContainer = styled.div`
+  ${props =>
+    props.theme.history.location.pathname.startsWith("/dev") && "height: 100%"};
+  .essential-container {
+    display: flex;
+    height: 100%;
+  }
+
+  .main-app-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .main-app-column {
+      background: transparent linear-gradient(297deg, #f2f3ff 0%, #ffffff 100%);
+      .developer-home {
+        padding: 11.5625em 7.125em 7em 6.625em;
+      }
+    }
+  }
+`;
