da3a028c0e3dba5eb0fb25ad90c8e8090cfd18e6
bejolo018 <bejolo018@gmail.com>
2019-08-29 22:36:49 -0500

    begun composing landing page to react component

---
+++ src/components/LandingStatic/Landing.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { connect } from 'react-redux'
+
+class Landing extends Component{
+    <div></div>
+}
