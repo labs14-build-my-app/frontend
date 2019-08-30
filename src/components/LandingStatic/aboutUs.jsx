b1203c16f699f8b73d13e1e56cbe42dff203bd15
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-08-29 20:39:49 -0700

    Create aboutUs.jsx

---
+++ src/components/LandingStatic/aboutUs.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class aboutUs extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			alfonso:[],
+			jacob:[],
+			ruben:[],
+			benji:[],
+			anotoly:[],
+			ark:[],
+		}
+	}
+	render() { 
+		return (
+			<div className="aboutUsContainer">
+				AboutUs Lives Here
+			</div>
+		);
+	}
+}
+ 
+export default aboutUs;
